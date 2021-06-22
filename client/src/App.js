38672f02ef451ca41e0ff0cc53d58c97df766294
Nadia Collado <collado.nadia@gmail.com>
2021-06-22 13:34:51 -0400

    bug: handleScroll running multiple times before resetting state

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,12 @@
 import './'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import getPins from './getPins'
 import "./App.css"
 
 function App() {
   const [startingRow, setStartingRow] = useState(0)
   const [pins, setPins] = useState([])
-  const [loading, setLoading] = useState(false)
   const loadedRowCount = 10
-
-  const handleScroll = event => {
-    if (window.document.body.offsetHeight <= window.pageYOffset + window.innerHeight){
-      console.log('reached the bottom')
-      setStartingRow(prev => prev + 10)
-    }
-  }
-
-  window.addEventListener('scroll', handleScroll)
 
   useEffect(() => {
       const fetchPins = () => {
@@ -24,9 +14,16 @@ function App() {
         console.log(startingRow, startingRow + loadedRowCount)
         setPins(prev => [...prev, ...newPins])
       }
-      console.log(startingRow, 'startingRow')
       fetchPins()
   }, [startingRow])
+
+  const handleScroll = event => {
+    if (window.document.body.offsetHeight <= window.pageYOffset + window.innerHeight){
+      setStartingRow(prev => prev + 10)
+    }
+  }
+
+  window.addEventListener('scroll', handleScroll)
   
   return (
     
@@ -36,8 +33,7 @@ function App() {
           PINFINITY
         </p>
         {pins} 
-    
-        <div>{loading && 'Loading...'}</div>
+  
       </header>
     </div>
   );
